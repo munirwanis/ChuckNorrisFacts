1bb057f603ead7e6cc5788b129c251cb3fcf81f5
Munir Wanis <fallen.angel.pp@gmail.com>
2018-05-18 16:27:12 -0300

    ➕  First commit

---
+++ ChuckNorrisFacts/ChuckNorrisFacts.h
@@ -0,0 +1,19 @@
+//
+//  ChuckNorrisFacts.h
+//  ChuckNorrisFacts
+//
+//  Created by Munir Wanis on 18/05/18.
+//  Copyright © 2018 Wanis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChuckNorrisFacts.
+FOUNDATION_EXPORT double ChuckNorrisFactsVersionNumber;
+
+//! Project version string for ChuckNorrisFacts.
+FOUNDATION_EXPORT const unsigned char ChuckNorrisFactsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChuckNorrisFacts/PublicHeader.h>
+
+
